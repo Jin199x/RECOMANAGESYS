5fa6a5e5139e209da93c07b05a56259706c36908
Jin199x <andylyon0107@gmail.com>
2025-10-14 10:11:02 +0800

    updated unitsForm, added unregistered tenant/caretaker

--- UnitsForm.Designer.cs
+++ UnitsForm.Designer.cs
@@ -31,14 +31,18 @@
             this.btnUnregisterSelected = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
             this.DGVUnits = new System.Windows.Forms.DataGridView();
+            this.DGVTCunregister = new System.Windows.Forms.DataGridView();
+            this.btnUnregisterTC = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.DGVUnits)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVTCunregister)).BeginInit();
             this.SuspendLayout();
             // 
             // btnUnregisterSelected
             // 
-            this.btnUnregisterSelected.Location = new System.Drawing.Point(159, 30);
+            this.btnUnregisterSelected.Location = new System.Drawing.Point(179, 38);
+            this.btnUnregisterSelected.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnUnregisterSelected.Name = "btnUnregisterSelected";
-            this.btnUnregisterSelected.Size = new System.Drawing.Size(199, 34);
+            this.btnUnregisterSelected.Size = new System.Drawing.Size(224, 42);
             this.btnUnregisterSelected.TabIndex = 1;
             this.btnUnregisterSelected.Text = "Unregister Units";
             this.btnUnregisterSelected.UseVisualStyleBackColor = true;
@@ -46,9 +50,10 @@
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(485, 30);
+            this.btnCancel.Location = new System.Drawing.Point(546, 38);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(90, 34);
+            this.btnCancel.Size = new System.Drawing.Size(101, 42);
             this.btnCancel.TabIndex = 2;
             this.btnCancel.Text = "cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -60,27 +65,59 @@
             this.DGVUnits.AllowUserToDeleteRows = false;
             this.DGVUnits.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.DGVUnits.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DGVUnits.Location = new System.Drawing.Point(12, 79);
+            this.DGVUnits.Location = new System.Drawing.Point(14, 97);
+            this.DGVUnits.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.DGVUnits.MultiSelect = false;
             this.DGVUnits.Name = "DGVUnits";
             this.DGVUnits.RowHeadersWidth = 51;
             this.DGVUnits.RowTemplate.Height = 24;
             this.DGVUnits.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.DGVUnits.Size = new System.Drawing.Size(776, 338);
+            this.DGVUnits.Size = new System.Drawing.Size(1206, 303);
             this.DGVUnits.TabIndex = 3;
+            // 
+            // DGVTCunregister
+            // 
+            this.DGVTCunregister.AllowUserToAddRows = false;
+            this.DGVTCunregister.AllowUserToDeleteRows = false;
+            this.DGVTCunregister.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DGVTCunregister.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVTCunregister.Location = new System.Drawing.Point(14, 481);
+            this.DGVTCunregister.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.DGVTCunregister.MultiSelect = false;
+            this.DGVTCunregister.Name = "DGVTCunregister";
+            this.DGVTCunregister.RowHeadersWidth = 51;
+            this.DGVTCunregister.RowTemplate.Height = 24;
+            this.DGVTCunregister.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.DGVTCunregister.Size = new System.Drawing.Size(1206, 303);
+            this.DGVTCunregister.TabIndex = 4;
+            // 
+            // btnUnregisterTC
+            // 
+            this.btnUnregisterTC.Location = new System.Drawing.Point(179, 431);
+            this.btnUnregisterTC.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnUnregisterTC.Name = "btnUnregisterTC";
+            this.btnUnregisterTC.Size = new System.Drawing.Size(224, 42);
+            this.btnUnregisterTC.TabIndex = 5;
+            this.btnUnregisterTC.Text = "Unregister Tenant/Caretaker";
+            this.btnUnregisterTC.UseVisualStyleBackColor = true;
+            this.btnUnregisterTC.Click += new System.EventHandler(this.btnUnregisterTC_Click);
             // 
             // UnitsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1236, 804);
+            this.Controls.Add(this.btnUnregisterTC);
+            this.Controls.Add(this.DGVTCunregister);
             this.Controls.Add(this.DGVUnits);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnUnregisterSelected);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "UnitsForm";
             this.Text = "UnitsForm";
             this.Load += new System.EventHandler(this.UnitsForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.DGVUnits)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVTCunregister)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -89,5 +126,7 @@
         private System.Windows.Forms.Button btnUnregisterSelected;
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.DataGridView DGVUnits;
+        private System.Windows.Forms.DataGridView DGVTCunregister;
+        private System.Windows.Forms.Button btnUnregisterTC;
     }
 }
--- UnitsForm.cs
+++ UnitsForm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.Windows.Forms;
@@ -15,6 +16,14 @@ namespace RECOMANAGESYS
         {
             InitializeComponent();
             _residentId = residentId;
+        }
+
+        private void UnitsForm_Load(object sender, EventArgs e)
+        {
+            InitializeDataGridView();
+            LoadUnitsForResident();
+            DGVUnits.SelectionChanged += DGVUnits_SelectionChanged;
+            DGVTCunregister.CellContentClick += DGVTCunregister_CellContentClick;
         }
 
         private void InitializeDataGridView()
@@ -33,7 +42,6 @@ namespace RECOMANAGESYS
             DGVUnits.Columns.Add("Status", "Status");
 
             DGVUnits.Columns["UnitID"].Visible = false;
-            DGVUnits.Size = new Size(850, 400);
             DGVUnits.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             DGVUnits.ColumnHeadersHeight = 45;
             DGVUnits.ColumnHeadersDefaultCellStyle = new DataGridViewCellStyle
@@ -50,18 +58,59 @@ namespace RECOMANAGESYS
                 SelectionBackColor = Color.CornflowerBlue,
                 SelectionForeColor = Color.White
             };
-
             DGVUnits.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(240, 240, 240);
-            DGVUnits.ScrollBars = ScrollBars.Both;
             DGVUnits.RowHeadersVisible = false;
             DGVUnits.GridColor = Color.LightGray;
+
+            DGVTCunregister.Columns.Clear();
+            DGVTCunregister.Columns.Add(new DataGridViewCheckBoxColumn
+            {
+                Name = "Select",
+                HeaderText = "Select",
+                Width = 70
+            });
+            DGVTCunregister.Columns.Add("ResidentID", "ResidentID");
+            DGVTCunregister.Columns.Add("FullName", "Full Name");
+            DGVTCunregister.Columns.Add("ResidencyType", "Residency Type");
+            DGVTCunregister.Columns.Add("Status", "Status");
+
+            DGVTCunregister.Columns["ResidentID"].Visible = false;
+            DGVTCunregister.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            DGVTCunregister.ColumnHeadersHeight = 45;
+            DGVTCunregister.ColumnHeadersDefaultCellStyle = new DataGridViewCellStyle
+            {
+                BackColor = Color.SteelBlue,
+                ForeColor = Color.White,
+                Font = new Font("Arial", 12F, FontStyle.Bold),
+                Alignment = DataGridViewContentAlignment.MiddleCenter
+            };
+            DGVTCunregister.EnableHeadersVisualStyles = false;
+            DGVTCunregister.DefaultCellStyle = new DataGridViewCellStyle
+            {
+                Font = new Font("Arial", 11F),
+                SelectionBackColor = Color.CornflowerBlue,
+                SelectionForeColor = Color.White
+            };
+            DGVTCunregister.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(240, 240, 240);
+            DGVTCunregister.RowHeadersVisible = false;
+            DGVTCunregister.GridColor = Color.LightGray;
+
+            DGVTCunregister.EditMode = DataGridViewEditMode.EditOnEnter;
+            DGVTCunregister.ReadOnly = false;
+            foreach (DataGridViewColumn col in DGVTCunregister.Columns)
+            {
+                if (col.Name != "Select")
+                    col.ReadOnly = true;
+            }
+
+            DGVTCunregister.CellContentClick += (s, e) =>
+            {
+                if (e.RowIndex >= 0 && e.ColumnIndex == DGVTCunregister.Columns["Select"].Index)
+                {
+                    DGVTCunregister.CommitEdit(DataGridViewDataErrorContexts.Commit);
+                }
+            };
         }
-        private void UnitsForm_Load(object sender, EventArgs e)
-        {
-            InitializeDataGridView();
-            LoadUnitsForResident();
-        }
-      
 
         private void LoadUnitsForResident()
         {
@@ -96,8 +145,159 @@ namespace RECOMANAGESYS
                 MessageBox.Show($"Error loading units: {ex.Message}", "Error",
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+            if (DGVUnits.SelectedRows.Count > 0)
+            {
+                int selectedUnitId = Convert.ToInt32(DGVUnits.SelectedRows[0].Cells["UnitID"].Value);
+                LoadTenantsAndCaretaker(selectedUnitId);
+            }
         }
 
+        private void DGVUnits_SelectionChanged(object sender, EventArgs e)
+        {
+            if (DGVUnits.SelectedRows.Count == 0) return;
+            int unitId = Convert.ToInt32(DGVUnits.SelectedRows[0].Cells["UnitID"].Value);
+            LoadTenantsAndCaretaker(unitId);
+        }
+
+        private void LoadTenantsAndCaretaker(int unitId)
+        {
+            try
+            {
+                using (SqlConnection conn = DatabaseHelper.GetConnection())
+                {
+                    conn.Open();
+                    string query = @"
+                        SELECT 
+                            r.ResidentID,
+                            CONCAT(r.FirstName, ' ', r.LastName) AS FullName,
+                            r.ResidencyType,
+                            CASE WHEN hu.IsCurrent = 1 THEN 'Active' ELSE 'Inactive' END AS Status
+                        FROM HomeownerUnits hu
+                        INNER JOIN Residents r ON hu.ResidentID = r.ResidentID
+                        WHERE hu.UnitID = @unitId
+                          AND r.ResidencyType IN ('Tenant', 'Caretaker')
+                        ORDER BY 
+                            CASE WHEN hu.IsCurrent = 1 THEN 0 ELSE 1 END, 
+                            r.ResidencyType,
+                            r.LastName,
+                            r.FirstName;";
+
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@unitId", unitId);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        DGVTCunregister.Rows.Clear();
+                        while (reader.Read())
+                        {
+                            DGVTCunregister.Rows.Add(false, reader["ResidentID"], reader["FullName"],
+                                reader["ResidencyType"], reader["Status"]);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading tenants/caretakers: {ex.Message}", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void DGVTCunregister_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+            if (DGVTCunregister.Columns[e.ColumnIndex].Name == "Select")
+            {
+                DGVTCunregister.CommitEdit(DataGridViewDataErrorContexts.Commit);
+            }
+        }
+
+
+      
+
+
+        private void UnregisterResident(int residentId, int unitId)
+        {
+            try
+            {
+                using (SqlConnection conn = DatabaseHelper.GetConnection())
+                {
+                    conn.Open();
+                    using (SqlTransaction tran = conn.BeginTransaction())
+                    {
+                        try
+                        {
+                            string deactivateLink = @"
+                                UPDATE HomeownerUnits 
+                                SET IsCurrent = 0, DateOfOwnershipEnd = GETDATE()
+                                WHERE ResidentID = @residentId AND UnitID = @unitId AND IsCurrent = 1";
+                            using (SqlCommand cmd = new SqlCommand(deactivateLink, conn, tran))
+                            {
+                                cmd.Parameters.AddWithValue("@residentId", residentId);
+                                cmd.Parameters.AddWithValue("@unitId", unitId);
+                                cmd.ExecuteNonQuery();
+                            }
+
+                            string unitType = "";
+                            string residencyType = "";
+
+                            using (SqlCommand cmd = new SqlCommand("SELECT UnitType FROM TBL_Units WHERE UnitID=@id", conn, tran))
+                            {
+                                cmd.Parameters.AddWithValue("@id", unitId);
+                                unitType = cmd.ExecuteScalar()?.ToString() ?? "";
+                            }
+
+                            using (SqlCommand cmd = new SqlCommand("SELECT ResidencyType FROM Residents WHERE ResidentID=@id", conn, tran))
+                            {
+                                cmd.Parameters.AddWithValue("@id", residentId);
+                                residencyType = cmd.ExecuteScalar()?.ToString() ?? "";
+                            }
+
+                            if (unitType.Equals("Apartment", StringComparison.OrdinalIgnoreCase) &&
+                                residencyType.Equals("Tenant", StringComparison.OrdinalIgnoreCase))
+                            {
+                                using (SqlCommand cmd = new SqlCommand(
+                                    "UPDATE TBL_Units SET AvailableRooms = AvailableRooms + 1 WHERE UnitID=@id", conn, tran))
+                                {
+                                    cmd.Parameters.AddWithValue("@id", unitId);
+                                    cmd.ExecuteNonQuery();
+                                }
+                            }
+
+                            string check = "SELECT COUNT(*) FROM HomeownerUnits WHERE ResidentID=@id AND IsCurrent=1";
+                            using (SqlCommand cmd = new SqlCommand(check, conn, tran))
+                            {
+                                cmd.Parameters.AddWithValue("@id", residentId);
+                                if ((int)cmd.ExecuteScalar() == 0)
+                                {
+                                    using (SqlCommand deactivate = new SqlCommand(
+                                        "UPDATE Residents SET IsActive=0, InactiveDate=GETDATE() WHERE ResidentID=@id", conn, tran))
+                                    {
+                                        deactivate.Parameters.AddWithValue("@id", residentId);
+                                        deactivate.ExecuteNonQuery();
+                                    }
+                                }
+                            }
+
+                            tran.Commit();
+                            MessageBox.Show("Resident successfully unregistered.", "Success",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        catch (Exception ex)
+                        {
+                            tran.Rollback();
+                            MessageBox.Show($"Error unregistering resident: {ex.Message}", "Error",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Critical error: {ex.Message}", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
 
         private void btnUnregisterSelected_Click(object sender, EventArgs e)
         {
@@ -120,8 +320,98 @@ namespace RECOMANAGESYS
                 return;
             }
 
-            DialogResult = DialogResult.OK;
-            Close();
+            using (SqlConnection conn = DatabaseHelper.GetConnection())
+            {
+                conn.Open();
+
+                foreach (int unitId in SelectedUnitIds)
+                {
+                    string checkActiveQuery = @"
+                SELECT COUNT(*) 
+                FROM HomeownerUnits hu
+                INNER JOIN Residents r ON hu.ResidentID = r.ResidentID
+                WHERE hu.UnitID = @unitId 
+                  AND hu.IsCurrent = 1 
+                  AND r.ResidencyType IN ('Tenant', 'Caretaker');";
+
+                    using (SqlCommand checkCmd = new SqlCommand(checkActiveQuery, conn))
+                    {
+                        checkCmd.Parameters.AddWithValue("@unitId", unitId);
+                        int activeCount = (int)checkCmd.ExecuteScalar();
+
+                        if (activeCount > 0)
+                        {
+                            MessageBox.Show(
+                                $"Unit ID {unitId} cannot be unregistered because there are still active tenants or caretakers.",
+                                "Cannot Unregister Unit",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning
+                            );
+                            return;
+                        }
+                    }
+                }
+
+                var result = MessageBox.Show(
+                    "Are you sure you want to unregister the selected units from this owner?",
+                    "Confirm Unregister",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+
+                if (result != DialogResult.Yes)
+                    return;
+
+                using (SqlTransaction tran = conn.BeginTransaction())
+                {
+                    try
+                    {
+                        foreach (int unitId in SelectedUnitIds)
+                        {
+                            string deactivateLink = @"
+                        UPDATE HomeownerUnits 
+                        SET IsCurrent = 0, DateOfOwnershipEnd = GETDATE()
+                        WHERE ResidentID = @residentId AND UnitID = @unitId AND IsCurrent = 1";
+                            using (SqlCommand cmd = new SqlCommand(deactivateLink, conn, tran))
+                            {
+                                cmd.Parameters.AddWithValue("@residentId", _residentId);
+                                cmd.Parameters.AddWithValue("@unitId", unitId);
+                                cmd.ExecuteNonQuery();
+                            }
+
+                            string checkOccupants = @"
+                        SELECT COUNT(*) FROM HomeownerUnits 
+                        WHERE UnitID = @unitId AND IsCurrent = 1";
+                            using (SqlCommand cmd = new SqlCommand(checkOccupants, conn, tran))
+                            {
+                                cmd.Parameters.AddWithValue("@unitId", unitId);
+                                int count = (int)cmd.ExecuteScalar();
+                                if (count == 0)
+                                {
+                                    using (SqlCommand cmd2 = new SqlCommand(
+                                        "UPDATE TBL_Units SET IsOccupied = 0 WHERE UnitID = @unitId", conn, tran))
+                                    {
+                                        cmd2.Parameters.AddWithValue("@unitId", unitId);
+                                        cmd2.ExecuteNonQuery();
+                                    }
+                                }
+                            }
+                        }
+
+                        tran.Commit();
+                        MessageBox.Show("Selected units successfully unregistered.", "Success",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        LoadUnitsForResident();
+                        DGVTCunregister.Rows.Clear();
+                    }
+                    catch (Exception ex)
+                    {
+                        tran.Rollback();
+                        MessageBox.Show($"Error unregistering units: {ex.Message}", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -130,9 +420,46 @@ namespace RECOMANAGESYS
             Close();
         }
 
-        private void lblInstruction_Click(object sender, EventArgs e)
+        private void btnUnregisterTC_Click(object sender, EventArgs e)
         {
+            if (DGVTCunregister.Rows.Count == 0 || DGVUnits.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Please select a unit and at least one tenant/caretaker to unregister.",
+                    "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
 
+            int unitId = Convert.ToInt32(DGVUnits.SelectedRows[0].Cells["UnitID"].Value);
+            List<int> selectedResidentIds = new List<int>();
+            foreach (DataGridViewRow row in DGVTCunregister.Rows)
+            {
+                bool isSelected = Convert.ToBoolean(row.Cells["Select"].Value ?? false);
+                if (isSelected)
+                {
+                    selectedResidentIds.Add(Convert.ToInt32(row.Cells["ResidentID"].Value));
+                }
+            }
+
+            if (selectedResidentIds.Count == 0)
+            {
+                MessageBox.Show("Please check at least one tenant or caretaker to unregister.",
+                    "No Selection", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var result = MessageBox.Show("Are you sure you want to unregister the selected tenants/caretakers?",
+                "Confirm Unregister", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                foreach (int residentId in selectedResidentIds)
+                {
+                    UnregisterResident(residentId, unitId);
+                }
+
+                LoadTenantsAndCaretaker(unitId);
+                LoadUnitsForResident();
+            }
         }
     }
 }
